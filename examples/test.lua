ea5bb4f7744d20ad0f33ba1f739a170abc29e9d8
mrDoctorWho <mrdoctorwho@gmail.com>
2014-10-03 23:36:33 +0700

    A little code ident changes

--- examples/test.lua
+++ examples/test.lua
@@ -5,28 +5,28 @@ require 'captcha'
 --- examples for lua-captcha
 
 ---- simple example
-mycaptcha = captcha.new()
-mycaptcha:font("./Vera.ttf")
-cap = mycaptcha:write("out1.png")
+mycaptcha = captcha.new() -- create the captcha object
+mycaptcha:font("./Vera.ttf") -- set font
+cap = mycaptcha:write("out1.jpg", 70) -- write the captcha to file named out1.jpg with quality percentage 70
 print(cap)
 
 
----- scribbled background lines
+---- scribbled background linesq
 mycaptcha = captcha.new()
 mycaptcha:font("./Vera.ttf")
-mycaptcha:scribble()
-cap = mycaptcha:write("out2.png")
+mycaptcha:scribble() -- scribble the captcha randomly
+cap = mycaptcha:write("out2.jpg", 70)
 print(cap)
 
 
 
 ---- with words and colors
-mycaptcha = captcha.new()
+mycaptcha = captcha.new() 
 mycaptcha:font("./Vera.ttf")
-mycaptcha:string("i love lua")
-mycaptcha:bgcolor(255, 201, 51)
-mycaptcha:fgcolor(255, 102, 51)
+mycaptcha:string("i love lua") -- add the text!
+mycaptcha:bgcolor(255, 201, 51) -- colorize background!
+mycaptcha:fgcolor(255, 102, 51) -- and foreground!
 mycaptcha:line(true)
-cap = mycaptcha:write("out3.png")
+cap = mycaptcha:write("out3.jpg", 70)
 print(cap)
 
